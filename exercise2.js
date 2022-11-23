82f8539a68add5851ade0c59ce7448ddcf934686
Semen <justpr0z.com.ru@gmail.com>
2022-11-23 19:00:57 +0300

    added 2 exercise(error)

---
+++ exercise2.js
@@ -0,0 +1,19 @@
+function simpleСomposite(number) {
+  if (number < 2 && number > 1000 && !isNaN(number)){
+    return 'Данные неверны';
+  }
+  else (number === 2) {
+    return 'Простое число';
+  }
+  let i = 2;
+  const limit = Math.sqrt(number);
+  while (i <= limit) {
+    if (number % i === 0) {
+      return 'Составное число';
+    }
+    i++;
+  }
+  return 'Простое число';
+};
+
+console.log(simpleСomposite(0));
