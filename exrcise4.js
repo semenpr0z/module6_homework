7230031a752fefe39bc9d26c1418fc031c744d47
Semen <justpr0z.com.ru@gmail.com>
2022-11-25 16:36:38 +0300

    added 4 exercise

---
+++ exrcise4.js
@@ -0,0 +1,12 @@
+function printNumbers (a, b) {
+    let i = a;
+    let timerId = setInterval(function(){
+      console.log(i);
+      if (i == b){
+        clearInterval(timerId);
+      }
+      i++;
+    }, 1000);
+  }
+  
+  printNumbers(5, 10)
